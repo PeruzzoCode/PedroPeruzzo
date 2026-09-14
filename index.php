1c7572004bc7c6518822df98903a73893d118c78
Pedro Peruzzo <peruzzo.code@gmail.com>
2026-09-14 15:26:24 -0300

    html

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-echo"<h1>Olá turma</h1>";
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PedroPeruzzo</title>
+</head>
+<body>
+    echo "<h1>OI</h1>"
+</body>
+</html>
