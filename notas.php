593f612754fca460e2be263cce38d2f6c2c0323b
Pedro Peruzzo <peruzzo.code@gmail.com>
2026-09-21 15:23:18 -0300

    add campos de nota

---
+++ notas.php
@@ -0,0 +1,72 @@
+<?php
+        $nome="";
+        $idade=0;
+        $nota1=0;
+        $nota2=0;
+        $nota3=0;
+        $nota4=0;
+        $nota5=0;
+        $media=0;
+        $resultado="";
+        
+
+        if ($_SERVER["REQUEST_METHOD"]=="POST"){
+            $nota1 = $_POST["nota1"];
+            $nota2 = $_POST["nota2"];
+            $nota3 = $_POST["nota3"];
+            $nota4 = $_POST["nota4"];
+            $nota5 = $_POST["nota5"];
+            
+
+            if ($media >=7){
+                $resultado = "Aprovado";
+            } 
+
+            elseif ($media <=4.9) { 
+                $resultado = "Reprovado";
+
+            }
+            
+            else {
+                $resultado = "de Recuperação";
+            }
+        }
+    ?>
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Calculadora de média</title>
+</head>
+<body>
+
+    <form method="POST">
+
+        <input type="text" id="name" name="name" placeholder="Digite seu nome">
+        <input type="number" id="idade" name="idade" placeholder="Digite sua idade">
+        <input type="number" id="nota1" name="nota1" placeholder="Digite sua nota da 1° prova:">
+        <input type="number" id="nota2" name="nota2" placeholder="Digite sua nota da 2° prova:">
+        <input type="number" id="nota3" name="nota3" placeholder="Digite sua nota da 3° prova:">
+        <input type="number" id="nota4" name="nota4" placeholder="Digite sua nota da 4° prova:">
+        <input type="number" id="nota5" name="nota5" placeholder="Digite sua nota da 5° prova:">
+
+
+
+        <button type="submit">Enviar</button>
+
+    </form>
+
+    <?php if($resultado != "") { ?>
+    
+        <h1>O <?= $nome ?> de <?= $idade ?> anos de idade, está <?= $resultado ?> .</h1>
+        
+        <?php  } ?>
+
+    
+        
+
+</body>
+</html>
