00ddb9e48a8d605817438c093aa8f402c352de00
Pedro Peruzzo <peruzzo.code@gmail.com>
2026-09-22 14:26:15 -0300

    CSS notas-desafio
    copiando o css do notas e colando no css do notas-desafio

--- notas-desafio.php
+++ notas-desafio.php
@@ -41,7 +41,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="notas-desafio.css">
     <title>Calculadora de média</title>
 </head>
 <body>
