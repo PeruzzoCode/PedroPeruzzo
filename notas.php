eddfde56ce080f5f8fa77595cf7a9e4a6194808a
Pedro Peruzzo <peruzzo.code@gmail.com>
2026-09-22 15:11:02 -0300

    Add limite de nota e idade no notas.php

--- notas.php
+++ notas.php
@@ -12,14 +12,21 @@
 
         if ($_SERVER["REQUEST_METHOD"]=="POST"){
             $nome = $_POST["name"];
-            $idade = $_POST["idade"];
-            $nota1 = $_POST["nota1"];
-            $nota2 = $_POST["nota2"];
-            $nota3 = $_POST["nota3"];
-            $nota4 = $_POST["nota4"];
-            $nota5 = $_POST["nota5"];
+            $idade = (int) $_POST["idade"];
+            $nota1 = (float) $_POST["nota1"];
+            $nota2 = (float) $_POST["nota2"];
+            $nota3 = (float) $_POST["nota3"];
+            $nota4 = (float) $_POST["nota4"];
+            $nota5 = (float) $_POST["nota5"];
             
-            $media = (($nota1 * 2) + ($nota2 * 3) + ($nota3 * 1) + ($nota4 * 1) + ($nota5 * 3)) /10 ;
+            if ($idade <= 0 || $nota1 < 0 || $nota1 >10 || $nota2 < 0 ||  $nota2 >10 || $nota3 < 0 || $nota3 >10 || $nota4 < 0 || $nota4 >10 || $nota5 < 0 || $nota5 >10 ) {
+                $resultado = "A nota deve ser entre 0 e 10. A idade deve ser maior que 0.";
+                
+            }
+
+            else {
+                $media = (($nota1 * 2) + ($nota2 * 3) + ($nota3 * 1) + ($nota4 * 1) + ($nota5 * 3)) /10 ;
+            }
 
             if ($media >=7){
                 $resultado = "Aprovado";
