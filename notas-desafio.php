07094405f37560411358a04623e30c404e4f2e4b
Pedro Peruzzo <peruzzo.code@gmail.com>
2026-09-22 14:22:33 -0300

    CSS idade.php
     Só copiei e colei o css do notas, para o css do idade.

--- notas-desafio.php
+++ notas-desafio.php
@@ -64,10 +64,10 @@
 
     <?php if($resultado != "") { ?>
     
-        <h1>Relatório do aluno:<?= $nome ?></h1>
-        <h2>Idade:<?= $idade ?></h2>
-        <h2>Status:<?= $resultado?></h2>
-        <h2>Média final:<?= $media?></h2>
+        <h1>Relatório do aluno: <?= $nome ?></h1>
+        <h2>Idade: <?= $idade ?></h2>
+        <h2>Status: <?= $resultado?></h2>
+        <h2>Média final: <?= $media?></h2>
         
         <?php  } ?>
 
