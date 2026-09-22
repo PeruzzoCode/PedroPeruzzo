8f911af74d1cff4585f8cda6290e604e9415e70b
Pedro Peruzzo <peruzzo.code@gmail.com>
2026-09-22 16:36:05 -0300

    ajustando o erro em vermelho

--- notas.php
+++ notas.php
@@ -77,7 +77,7 @@
 
     </form>
 
-    <h1 style="color: red;"><?= $erro ?></h1>
+    
 
     <?php if($resultado != "") { ?>
     
@@ -86,7 +86,7 @@
         <h2>Status: <?= $resultado?></h2>
         <h2>Falta <?= $pontos?> pontos para atingir a média.</h2>
         <h2>Média final: <?= $media?></h2>
-        <h1><?= $erro?></h1>
+        <h1 style="color: red;"><?= $erro ?></h1>
         
         <?php  } ?>
 
